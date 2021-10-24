28be8f42998790a38521f2822b82fd8a40c90f3d
Anshul Sharma <89918968+CoderAnshul@users.noreply.github.com>
2021-10-24 20:44:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+
+  if (counter > 5) {
+    counter = 1;
+  }
+}, 5000);
+
+// video redendering done
+
+
+let imgbase = document.querySelector(".img-base");
+let videoView = document.querySelector(".video-view");
+const videoproject = document.querySelector(".video-project");
+
+
+document.querySelectorAll(".video-project img").forEach((vid)=>{
+
+  vid.onclick =() =>{
+    videoView.classList.add("active");
+    document.querySelector('.video-view video').src = vid.getAttribute("data-target") ;
+  }
+});
+
+function popupproject() {
+  videoView.classList.remove("active");
+}
+
+
+// rendering  all  images  in  main-img  div  throught  box-img 
+document.querySelectorAll(".box-img img").forEach((img)=>{
+
+  img.onclick =() =>{
+    document.querySelector('.main-img img').src = img.getAttribute('src') ;
+  }
+});
+
+
+
+var signup  =  document.querySelector("#signup");
+var  signup_page = document.querySelector(".signup-page");
+var signup_box = document.querySelector(".signup-box");
+var signin  =  document.querySelector("#signin");
+
+signup.addEventListener("click",()=>{
+   signup_page.classList.toggle("activated");
+   signup_box.classList.toggle("deactivate");
+});
+
+signin.addEventListener("click",()=>{
+  signup_page.classList.remove("activated");
+  signup_box.classList.add("deactivate");
+});
